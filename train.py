18c69ddb0c851136702ce9e7171d1cd905f6923e
Handsome Zebra <aaahchi@gmail.com>
2018-07-17 10:16:46 -0700

    Refine train.py.

--- train.py
+++ train.py
@@ -6,30 +6,34 @@ import os.path as op
 
 from allennlp.commands import main
 
-config_file = "experiments/quora_bimpm_cosine_word_char.json"
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python train.py <input_config_file>")
+        sys.exit(0)
 
-# Specify overrides
-overrides = json.dumps({
-	"iterator": {"batch_size": 64},
-    "trainer": {"cuda_device": 0}
-})
+    config_file = sys.argv[1]
 
-# Specify output dir according to current time
-experiment_name = op.splitext(op.basename(config_file))[0]
-time_stamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
-serialization_dir = "./output_%s_%s" % (experiment_name, time_stamp) 
+    # Specify overrides
+    overrides = json.dumps({
+        "iterator": {"batch_size": 64},
+        "trainer": {"cuda_device": 0}
+    })
 
+    # Specify output dir according to current time
+    experiment_name = op.splitext(op.basename(config_file))[0]
+    time_stamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+    serialization_dir = "./output_%s_%s" % (experiment_name, time_stamp) 
 
-# Assemble the command into sys.argv
-sys.argv = [
-    "allennlp",  # command name, not used by main
-    "train",
-    config_file,
-    "-s", serialization_dir,
-    "--include-package", "hznlp",
-    "-o", overrides,
-]
+    # Assemble the command into sys.argv
+    sys.argv = [
+        "allennlp",  # command name, not used by main
+        "train",
+        config_file,
+        "-s", serialization_dir,
+        "--include-package", "hznlp",
+        "-o", overrides,
+    ]
 
-logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)
 
-main()
+    main()
