408a96901823095d47911ce6ca3aa789d2a7f4cb
Handsome Zebra <aaahchi@gmail.com>
2018-11-06 09:18:12 -0800

    Update sequence classification by separating seq2seq and seq2vec.

--- my_library/models/sequence_classification.py
+++ my_library/models/sequence_classification.py
@@ -7,7 +7,7 @@ from torch import nn
 import torch.nn.functional as F
 
 from allennlp.data import Vocabulary
-from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder
+from allennlp.modules import FeedForward, Seq2SeqEncoder, Seq2VecEncoder, TextFieldEmbedder
 from allennlp.models.model import Model
 from allennlp.nn import InitializerApplicator, RegularizerApplicator
 from allennlp.nn import util
@@ -19,7 +19,7 @@ class SequenceClassification(Model):
     def __init__(self, vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
                  embedding_dropout: float,
-                 encoder: Seq2VecEncoder,
+                 seq2seq_encoder: Seq2SeqEncoder,
                  classifier_feedforward: FeedForward,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
@@ -28,7 +28,8 @@ class SequenceClassification(Model):
         self.text_field_embedder = text_field_embedder
         self._embedding_dropout = nn.Dropout(embedding_dropout)
         self.num_classes = self.vocab.get_vocab_size("label")
-        self.encoder = encoder
+        self.seq2seq_encoder = seq2seq_encoder
+        self.self_attentive_pooling_projection = nn.Linear(seq2seq_encoder.get_output_dim(), 1)
         self.classifier_feedforward = classifier_feedforward
 
         self.metrics = {
@@ -48,9 +49,14 @@ class SequenceClassification(Model):
         mask_tokens = util.get_text_field_mask(tokens)
         embedded_tokens = self.text_field_embedder(tokens)
         dropped_embedded_tokens = self._embedding_dropout(embedded_tokens)
-        encoded_tokens = self.encoder(dropped_embedded_tokens, mask_tokens)
 
-        logits = self.classifier_feedforward(encoded_tokens)
+        encoded_tokens = self.seq2seq_encoder(dropped_embedded_tokens, mask_tokens)
+
+        self_attentive_logits = self.self_attentive_pooling_projection(encoded_tokens).squeeze(2)
+        self_weights = util.masked_softmax(self_attentive_logits, mask_tokens)
+        encoding_result = util.weighted_sum(encoded_tokens, self_weights)
+
+        logits = self.classifier_feedforward(encoding_result)
         class_probabilities = F.softmax(logits, dim=-1)
 
         output_dict = {'logits': logits, 'class_probabilities': class_probabilities}
