0e0be6a196fbf52a0d61237d4d2cbda3ff0cac26
Handsome Zebra <aaahchi@gmail.com>
2019-04-07 22:00:18 -0700

    refine csv file reader and add sentiment classification config

--- my_library/models/csv_file_reader.py
+++ my_library/models/csv_file_reader.py
@@ -11,6 +11,7 @@ from allennlp.data.dataset_readers.dataset_reader import DatasetReader
 from allennlp.data.fields import LabelField, TextField
 from allennlp.data.instance import Instance
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
+from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -23,20 +24,22 @@ class CsvFileDatasetReader(DatasetReader):
                  tokenizer: Tokenizer = None,
                  token_indexers: Dict[str, TokenIndexer] = None) -> None:
         super().__init__(lazy)
-        self._tokenizer = tokenizer or WordTokenizer()
+        self._tokenizer = tokenizer or WordTokenizer(JustSpacesWordSplitter())
         self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
 
     @overrides
     def _read(self, file_path):
         with open(cached_path(file_path), "r") as data_file:
-            tsv_in = csv.reader(data_file, delimiter='\t')
-            for row in tsv_in:
+            csv_in = csv.reader(data_file)
+            for row in csv_in:
                 if len(row) == 2:
-                    yield self.text_to_instance(text=row[1], label=row[0])
+                    yield self.text_to_instance(text=row[0], label=row[1])
 
     @overrides
     def text_to_instance(self, text: str, label: str = None) -> Instance:  # type: ignore
         # pylint: disable=arguments-differ
+        if len(text) == 0:
+            text = "."
         tokenized_text = self._tokenizer.tokenize(text)
         text_field = TextField(tokenized_text, self._token_indexers)
         fields = {'tokens': text_field}
